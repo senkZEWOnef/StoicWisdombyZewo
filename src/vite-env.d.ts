2c7c6b534df2cf69e55dd6aeb5e700253e6c1b5f
Ralph Ulysse <ralphulysse@Ralphs-MacBook-Air.local>
2025-10-20 22:06:08 -0400

    Additional TypeScript fixes for production build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,3 +10,19 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+// CSS modules
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+// React DOM client fix for newer versions
+declare module 'react-dom/client' {
+  export interface Root {
+    render(children: React.ReactNode): void;
+    unmount(): void;
+  }
+  export function createRoot(container: Element | DocumentFragment): Root;
+  export function hydrateRoot(container: Element | DocumentFragment, initialChildren: React.ReactNode): Root;
+}
